0866f74f6d7c4e4d6de14954e2c8c2948e638745
Balrog <Balrog@BALROG-PC>
2016-06-23 17:04:38 +0200

    New Scripts

---
+++ Scripts/Camera/Camera_Zoom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Zoom : MonoBehaviour {
+
+    public float zoomSize = 5;
+
+	void Update () {
+        
+        if(Input.GetAxis("Mouse ScrollWheel") > 0 )
+        {
+            if (zoomSize > 4)
+                zoomSize -= 1;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (zoomSize < 8)
+                zoomSize += 1;
+        }
+
+        GetComponent<Camera>().orthographicSize = zoomSize;
+    }
+
+}
